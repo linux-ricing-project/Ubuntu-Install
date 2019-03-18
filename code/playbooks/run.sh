54b24b58b384e9fb16e0d0b2013ec8da9fd1b4e9
Frank Junior <frankcbjunior@gmail.com>
2019-03-18 17:24:42 -0300

    add installation by Ansible

---
+++ code/playbooks/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# ============================================
+# mostrar o banner inicial
+# ============================================
+show_header(){
+cat << "HEADER"
+   __  ____                __           ____           __        ____
+  / / / / /_  __  ______  / /___  __   /  _/___  _____/ /_____ _/ / /
+ / / / / __ \/ / / / __ \/ __/ / / /   / // __ \/ ___/ __/ __ `/ / /
+/ /_/ / /_/ / /_/ / / / / /_/ /_/ /  _/ // / / (__  ) /_/ /_/ / / /
+\____/_.___/\__,_/_/ /_/\__/\__,_/  /___/_/ /_/____/\__/\__,_/_/_/
+
+HEADER
+}
+
+# ============================================
+# Fazendo as atualizações iniciais
+# ============================================
+init_updates(){
+  # updagrade inicial, por volta de uns 300 MB
+  echo "==========================================="
+  echo "Fazendo as atualizações iniciais..."
+  echo "==========================================="
+  sudo apt -y upgrade
+  sudo apt -y dist-upgrade
+  sudo apt -y full-upgrade
+
+	echo "==========================================="
+  echo "instalando o ansible..."
+  echo "==========================================="
+	sudo apt -y install software-properties-common
+	sudo apt-add-repository --yes --update ppa:ansible/ansible
+	sudo apt -y install ansible curl wget
+}
+
+show_header
+init_updates
+
+# runnind playbook
+echo "==========================================="
+echo "running ansible job"
+echo "==========================================="
+ansible-playbook --ask-become-pass main.yaml
