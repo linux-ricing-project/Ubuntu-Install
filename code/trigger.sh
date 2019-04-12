02039eae484599abfc28ceab044b71213e866c9f
Frank Junior <frankcbjunior@gmail.com>
2019-04-12 17:35:57 -0300

    url de teste

--- code/trigger.sh
+++ code/trigger.sh
@@ -2,7 +2,7 @@
 
 root_location=$(pwd)
 
-wget https://github.com/frankjuniorr/ubuntu_install/archive/master.zip -O ubuntu_install.zip
+wget https://github.com/frankjuniorr/ubuntu_install/archive/dev.zip -O ubuntu_install.zip
 unzip ubuntu_install.zip
 rm -rf ubuntu_install.zip
 
