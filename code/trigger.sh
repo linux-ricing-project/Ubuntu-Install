dd8167e2b5d79e3b5d15a3502fd6b3893c96d5d0
Frank Junior <frankcbjunior@gmail.com>
2019-04-15 10:43:36 -0300

    add blur image in the lockscreen wallpaper

--- code/trigger.sh
+++ code/trigger.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 root_location=$(pwd)
+# Variável para fins de teste
+branch="dev"
 
-wget https://github.com/frankjuniorr/ubuntu_install/archive/dev.zip -O ubuntu_install.zip
+wget https://github.com/frankjuniorr/ubuntu_install/archive/${branch}.zip -O ubuntu_install.zip
 unzip ubuntu_install.zip
 rm -rf ubuntu_install.zip
 
-cd ubuntu_install-dev/code/playbooks
+cd ubuntu_install-${branch}/code/playbooks
 clear
 
 ./run.sh
 
-rm -rf "${root_location}/ubuntu_install-dev"
+rm -rf "${root_location}/ubuntu_install-${branch}"
