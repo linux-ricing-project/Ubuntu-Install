c0f693dd59174dd7bb23156d93398c98c0e1e541
Frank Junior <frankcbjunior@gmail.com>
2019-08-06 15:46:11 -0300

    update trigger.sh

--- code/trigger.sh
+++ code/trigger.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 root_location=$(pwd)
-# Variável para fins de teste
-branch="dev"
 
-wget https://github.com/frankjuniorr/ubuntu_install/archive/${branch}.zip -O ubuntu_install.zip
+wget https://github.com/frankjuniorr/ubuntu_install/archive/master.zip -O ubuntu_install.zip
 unzip ubuntu_install.zip
 rm -rf ubuntu_install.zip
 
-cd ubuntu_install-${branch}/code/base
+cd ubuntu_install-master/code/base
 clear
 
 ./run.sh
 
-rm -rf "${root_location}/ubuntu_install-${branch}"
+rm -rf "${root_location}/ubuntu_install-master"
