551bd5ab635d689714e3fdefc7af2bc0d29d7291
Frank Junior <frankcbjunior@gmail.com>
2023-03-01 20:27:57 -0300

    update spotify installation

--- code/base/src/2-install-packages.sh
+++ code/base/src/2-install-packages.sh
@@ -47,7 +47,7 @@ install_spotify(){
     echo " Install Spotify"
     echo "############################################"
 
-    curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+    curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
     sudo apt update
     sudo apt install -y spotify-client
